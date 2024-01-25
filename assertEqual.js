eaf90c88feae8589cf348c5e63a904bf38a4a070
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-24 20:47:27 -0700

    corrected the Assertion failed line to be !== instead of === and removed an additional console.log which was unneccessary

--- assertEqual.js
+++ assertEqual.js
@@ -45,11 +45,9 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed ❌: ${actual} === ${expected}`);
+    console.log(`Assertion Failed ❌: ${actual} !== ${expected}`);
   }
-};
 
-console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
 // TEST CODE
 assertEqual("hi", "hi"); // comparing identical strings - should pass
 assertEqual("hi", "hello"); // comparing non-identical strings - should fail
