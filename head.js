9e845ce7dcf4e023cd03c21b490fc653facf75ca
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-25 19:27:47 -0700

    Removed a paragraph of unnecessary code. Added more test arrays.

--- head.js
+++ head.js
@@ -1,26 +1,8 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`Assertion Failed ❌: ${actual} !== ${expected}`);
-//   }
-
-// // TEST CODE
-// assertEqual("hi", "hi"); // comparing identical strings - should pass
-// assertEqual("hi", "hello"); // comparing non-identical strings - should fail
-// assertEqual(1, 1); // comparing identical numbers - should pass
-// assertEqual(1, 3); // comparing non-identical numbers - should fail
-
-
-
-// //Instruction
+// Instructions:
 // Create a function head which returns the first item in the array.
 
 // The head function should not return the first element as an array.
 // It should simply return the element itself.
-
-
-// Instruction
 // Use assertEqual to write test cases for various scenarios.
 
 // The tests can be written below the definition of your function.
@@ -49,3 +31,4 @@ const assertEqual = function(actual, expected) {
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5,6,7]), 6);
+assertEqual(head([]), 6);
