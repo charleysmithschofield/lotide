a3e73c2a66251a048d5282206e85558882925a85
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-28 15:21:14 -0700

    corrected test arrays expected values

--- middle.js
+++ middle.js
@@ -76,4 +76,4 @@ assertArraysEqual(middle([2, 3, 4, 5, 7]), [4]); // => [4]
 
 // Test cases for arrays with even number of elements
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), (3, 4)); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
