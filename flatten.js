36a096135cb42a065025b36a7802411449b219d5
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-28 14:48:10 -0700

    corrected last flatten test array

--- flatten.js
+++ flatten.js
@@ -52,4 +52,4 @@ assertArraysEqual([2,2], [1,2]);
 assertArraysEqual([],[1,2]);
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
 assertArraysEqual(flatten([[1, 3], [5, 4], 5, [6]]), [1, 3, 5, 6, 9]); // => [1, 3, 4, 6, 9]
-assertArraysEqual(flatten([[1, 4], []]), 6, [4]); // => [6]
+assertArraysEqual(flatten([[1, 4], []]), [1, 4]); // => [6]
