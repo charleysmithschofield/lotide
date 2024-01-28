2abec54398e7520bd52234251c2d68275af5f38a
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-27 23:34:13 -0700

    adding assertArraysEqual file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -29,14 +29,14 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 }
-
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual.toString()} === ${expected.toString()}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Assertion Failed: ${actual.toString()} !== ${expected.toString()}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
 assertArraysEqual([1, 2], [1,2]); 
 assertArraysEqual([2,2], [1,2]);
 assertArraysEqual([],[1,2]);
