5a364429c3b4b362addfd0d19a77129d6e585a0a
charleysmithschofield <charleysmithschofield@gmail.com>
2024-01-29 21:30:56 -0700

    removed unnecessary script and modified tests

--- letterPositions.js
+++ letterPositions.js
@@ -27,12 +27,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(actual, expected) {
-  // This check ensures that neither actual nor expected is undefined before comparing them.
-  // if (actual === undefined || expected === undefined) {
-  //   console.log("❌ Assertion Failed: one of the arguments is undefined");
-  //   return; // Exit the function if any of the arguments is undefined.
-  // }
-  
+
   // Only proceed to compare arrays if both actual and expected are defined.
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -45,7 +40,7 @@ const letterPositions = function(sentence) {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
-    let char = sentence[i];
+    const char = sentence[i];
     if (char !== ' ') {
       if (!results[char]) {
         results[char] = [];
@@ -56,16 +51,16 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions("hello"));
+let result = letterPositions("lighthouse in the house");
 
-assertArraysEqual(letterPositions("lighthouse in the house").l, [0]);
-assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]);
-assertArraysEqual(letterPositions("lighthouse in the house").g, [2]);
-assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
-assertArraysEqual(letterPositions("lighthouse in the house").t, [4, 14]);
-assertArraysEqual(letterPositions("lighthouse in the house").o, [6, 19]);
-assertArraysEqual(letterPositions("lighthouse in the house").u, [7, 20]);
-assertArraysEqual(letterPositions("lighthouse in the house").s, [8, 21]);
-assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
-assertArraysEqual(letterPositions("lighthouse in the house").n, [12]);
+assertArraysEqual(result.l, [0]);
+assertArraysEqual(result.i, [1, 11]);
+assertArraysEqual(result.g, [2]);
+assertArraysEqual(result.h, [3, 5, 15, 18]);
+assertArraysEqual(result.t, [4, 14]);
+assertArraysEqual(result.o, [6, 19]);
+assertArraysEqual(result.u, [7, 20]);
+assertArraysEqual(result.s, [8, 21]);
+assertArraysEqual(result.e, [9, 16, 22]);
+assertArraysEqual(result.n, [12]);
   
