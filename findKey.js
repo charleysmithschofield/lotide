461c52e9d00e4e755508bc87ad97e4d90780450c
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-04 15:57:34 -0700

    corrected test code

--- findKey.js
+++ findKey.js
@@ -58,4 +58,4 @@ assertEqual(findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+}, x => x.stars === 2), "noma"); // => "noma"
