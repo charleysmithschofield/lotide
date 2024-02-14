86c6c1dfef09cd0336f7a504bdcd6d9e289c3419
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 21:05:56 -0700

    added a comment above the function definition to define what the function does

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,5 @@
 
+// assertEqual is a function that compares an actual value to an expected value and will show if that assertion passed or failed.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
