7df6035f7504949704f7173deebe43f0da2121e8
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 20:49:59 -0700

    moved head.js test code into a test file

--- head.js
+++ head.js
@@ -12,23 +12,12 @@
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
+const assertEqual = require('./assertEqual');
 
 // Define function head:
 const head = function(arr) {
   return arr[0];
 };
 
-// Define function assertEqual
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed ❌: ${actual} !== ${expected}`);
-  }
-};
 
-// TEST CODE:
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 6);
-assertEqual(head([]), 6);
+module.exports = head;
