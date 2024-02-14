3f70e7af9c89fa1945a1b3084fc0099573908026
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 20:46:11 -0700

    adding assertEqual.js file now that I have separated the test code into a test file

--- assertEqual.js
+++ assertEqual.js
@@ -49,8 +49,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("hi", "hi"); // comparing identical strings - should pass
-assertEqual("hi", "hello"); // comparing non-identical strings - should fail
-assertEqual(1, 1); // comparing identical numbers - should pass
-assertEqual(1, 3); // comparing non-identical numbers - should fail
+module.exports = assertEqual;
