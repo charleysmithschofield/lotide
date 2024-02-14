06f83ebb48799b21fae8cca7b6dc5ff61ad2b76a
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 21:30:19 -0700

    split tail.js file test cases into its own test file

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
 
-// Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
+// requires assertEqual function for tests
+const assertEqual = require('./assertEqual');
 
-// Your function should not modify the array that is passed in. It should return a new array.
-
-// Warning
-// Our assertEqual function is too simple to compare array values. JavaScript doesn't allow the use of === or == to compare two arrays.
-
-// Try out the equality checks between arrays in the node REPL to confirm the results shown below.
-
-// [1, 2, 3] === [1, 2, 3] // => false
-// [1, 2, 3] == [1, 2, 3] // => false
-// // Therefore, we cannot expect this assertion to behave correctly:
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
+// tail is a function which returns the "tail" of an array. The "tail" is everything except for the first element.
 
 // Define the function called tail.
 const tail = function(arr) {
@@ -24,40 +12,6 @@ const tail = function(arr) {
   // Return the array minus the head of the array.
   return arr.slice(1);
 };
-const assertEqual = function(actual, expected) {
-  
-  // Check the length of the arrays instead of direct comparison
-  if (actual.length !== expected.length) {
-    console.log(`Assertion Failed ❌: ${actual} !== ${expected}`);
-    return;
-  }
 
-  // Use a for loop to iterate through each array comparing each element.
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`Assertion Failed ❌: ${actual} !== ${expected}`);
-      return;
-    }
-  }
-
-  console.log(`Assertion Passed ✅: ${actual} === ${expected}`);
-};
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const wordsTail = tail(words);
-assertEqual(wordsTail.length, 2); // Check the length of the returned array
-assertEqual(words.length, 3); // Check that the original array has not changed
-
-// Test Case 2: Array with one element, yields an empty array.
-const wordsSingle = ["Hello"];
-const singleTail = tail(wordsSingle);
-assertEqual(singleTail.length, 0); // Check the length of the returned array
-assertEqual(wordsSingle.length, 1); // Check that the original array has not changed
-
-// Test Case 3: Empty array, yields an empty array for its tail.
-const emptyArray = [];
-const emptyArrayTail = tail(emptyArray);
-assertEqual(emptyArrayTail.length, 0); // Check the length of the returned array
-assertEqual(emptyArray.length, 0); // Check that the original array has not changed
+// exports tail
+module.exports = tail;
