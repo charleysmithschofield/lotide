924b23cd97700330d5ecacac7c9838b1850f342e
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 21:04:31 -0700

    removed duplicate test code

--- test/headTest.js
+++ test/headTest.js
@@ -4,5 +4,4 @@ const head = require('../head');
 // TEST CODE:
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 5);
 assertEqual(head([]), undefined);
