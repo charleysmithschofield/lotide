875c55c114ce99ed64a106399eb82ea364e3b67d
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 21:07:28 -0700

    added a comment to tell the user what this function does

--- head.js
+++ head.js
@@ -1,5 +1,7 @@
 
-// Define function head:
+// Head is a function that will return the first element of an array, also known as the "head".
+
+// Define head function
 const head = function(arr) {
   return arr[0];
 };
