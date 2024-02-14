58ccba59041bf6bad89c2bf7e62e751b66c2a246
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-13 20:56:04 -0700

    removed commented instructions to make this file cleaner

--- head.js
+++ head.js
@@ -1,16 +1,3 @@
-// Instructions:
-// Create a function head which returns the first item in the array.
-
-// The head function should not return the first element as an array.
-// It should simply return the element itself.
-// Use assertEqual to write test cases for various scenarios.
-
-// The tests can be written below the definition of your function.
-
-// Here are some test assertions for you to implement and make pass:
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 const assertEqual = require('./assertEqual');
 
