089278d7c481c8d99d4944e1d4c7a21517b996c2
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-14 19:38:15 -0700

    refactored eqArrays.js file to separate tests into its own file

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,7 @@
-// Challenge
-// Instruction
 
-// Implement a function eqArrays which takes in two arrays and returns true or false,
-// based on a perfect match.
+const assertEqual = require('./assertEqual');
 
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-
-// Instruction
-// Use assertEqual to write test cases for various scenarios.
-
-// The tests can be written below the definition of your function.
-// Here's one example to get you started.
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// Please write out the rest of the assertions.
+// The function eqArrays takes in two arrays and returns true or false, based on a perfect match.
 
 // Define function called assertEqual that takes in two arrays as arguments.
 const eqArrays = function(arr1, arr2) {
@@ -34,19 +17,5 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-// Use assertEqual function to test if the actual and expected are the same.
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 3, 5], [3, 5, 7]), false); // => should PASS
-assertEqual(eqArrays([2, 5, 7], [2, 3, 7]), false); // => should PASS
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 9]), false); // => should PASS
-assertEqual(eqArrays(['chicken', 2, 2], [3, 0, 2]), false); // => should PASS
-
+// exports eqArrays function
+module.exports = eqArrays;
