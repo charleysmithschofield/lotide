c304b8a2a7e9c64f654543c334019f7b6500b8da
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-14 21:40:59 -0700

    changes middle tests using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,37 @@
 
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
-// TEST CODE
-assertArraysEqual([1, 2], [1,2]); // should pass, arrays are equal
-assertArraysEqual([2,2], [1,2]); // should fail, arrays are not equal
-assertArraysEqual([],[1,2]); // should fail, arrays are not equal
+// Tests if the middle function returns the middle element in an array, while leaving the original array unchanged.
 
-// Test cases for arrays with one or two elements
-assertArraysEqual(middle([1]), []); // shoud pass, empty array as there's only one element
-assertArraysEqual(middle([1, 3]), []); // should pass, empty array as there's only two elements
+// Test Cases:
+describe("#middle", () => {
+  it("returns the middle elements of an array, leaving the original array unchanged", () => {
+    // Test cases for arrays with one or two elements
+    assert.deepEqual(middle([1]), []); // shoud pass, empty array as there's only one element
+    assert.deepEqual(middle([1, 3]), []); // should pass, empty array as there's only two elements
+  });
+});
 
-// Test cases for arrays with odd numer of elements
-assertArraysEqual(middle([1, 2, 4]), [2]); // => should pass, middle element is 2
-assertArraysEqual(middle([2, 3, 4, 5, 7]), [4]); // should pass, middle element is 4
+describe("#middle", () => {
+  it("returns the middle elements of an array, leaving the original array unchanged", () => {
+    // // Test cases for arrays with odd numer of elements
+    assert.deepEqual(middle([1, 2, 4]), [2]); // => should pass, middle element is 2
+    assert.deepEqual(middle([2, 3, 4, 5, 7]), [4]); // should pass, middle element is 4
+  });
+});
 
-// Test cases for arrays with even number of elements
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass, middle elements are 2 and 3
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should pass, middle elements are 3 and 4
+describe("#middle", () => {
+  it("returns the middle elements of an array, leaving the original array unchanged", () => {
+    // Test cases for arrays with even number of elements
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass, middle elements are 2 and 3
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should pass, middle elements are 3 and 4
+  });
+});
+
+
+
+
+
+
