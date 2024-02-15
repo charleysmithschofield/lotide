731e104688ea61be7093f59442cb866ba69d6feb
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-14 21:51:10 -0700

    made each it description more specific to each test case

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,12 +12,12 @@ describe("#middle", () => {
     assert.deepEqual(middle([1]), []); // shoud pass, empty array as there's only one element
     assert.deepEqual(middle([1, 3]), []); // should pass, empty array as there's only two elements
   });
-  it("returns the middle elements of an array, leaving the original array unchanged", () => {
-    // // Test cases for arrays with odd numer of elements
+  it("returns the middle element(s) for array with an odd number of elements, leaving the original array unchanged", () => {
+    // // Test cases for arrays with odd number of elements
     assert.deepEqual(middle([1, 2, 4]), [2]); // => should pass, middle element is 2
     assert.deepEqual(middle([2, 3, 4, 5, 7]), [4]); // should pass, middle element is 4
   });
-  it("returns the middle elements of an array, leaving the original array unchanged", () => {
+  it("returns the middle elements for an array with an even number of elements, leaving the original array unchanged", () => {
     // Test cases for arrays with even number of elements
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass, middle elements are 2 and 3
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should pass, middle elements are 3 and 4
