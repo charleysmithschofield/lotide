0148c532a0ce9cad4223575c7d328be8eaa6ca17
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-15 20:05:58 -0700

    removed map function as required

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -33,7 +32,6 @@ module.exports = {
   findKeyByValue: findKeyByValue,
   flatten: flatten,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 };
